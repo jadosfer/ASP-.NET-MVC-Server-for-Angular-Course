bb9e379ab3d350d713e7c73db1a357e195cc07b5
javierfm <jadosfer@hotmail.com>
2021-03-11 13:56:57 -0300

    035 Search [Asp.Net Core MVC]

--- MvcTaskManager/Controllers/ProjectsController.cs
+++ MvcTaskManager/Controllers/ProjectsController.cs
@@ -19,6 +19,23 @@ namespace MvcTaskManager.Controllers
             return projects;
         }
 
+        [HttpGet]
+        [Route("api/projects/search/{searchby}/{searchtext}")]
+        public List<Project> Search(string searchBy, string searchText)
+        {
+            TaskManagerDbContext db = new TaskManagerDbContext();
+            List<Project> projects = null;
+            if (searchBy == "ProjectID")
+                projects = db.Projects.Where(temp => temp.ProjectID.ToString().Contains(searchText)).ToList();
+            else if (searchBy == "ProjectName")
+                projects = db.Projects.Where(temp => temp.ProjectName.Contains(searchText)).ToList();
+            else if (searchBy == "DateOfStart")
+                projects = db.Projects.Where(temp => temp.DateOfStart.ToString().Contains(searchText)).ToList();
+            else if (searchBy == "TeamSize")
+                projects = db.Projects.Where(temp => temp.TeamSize.ToString().Contains(searchText)).ToList();
+            return projects;
+        }
+
         [HttpPost]
         [Route("api/projects")]
         public Project Post([FromBody] Project project)
