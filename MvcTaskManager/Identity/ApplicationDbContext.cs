7ba449bbd167ed49911c4662b857fb7be4c50355
javierfm <jadosfer@hotmail.com>
2021-05-04 22:29:57 -0300

    132 Task Priorities Master nota:agregamos clase TaskPriority, creamos controller con los metodos CRUD y sembramos la DB

--- MvcTaskManager/Identity/ApplicationDbContext.cs
+++ MvcTaskManager/Identity/ApplicationDbContext.cs
@@ -18,6 +18,7 @@ namespace MvcTaskManager.Identity
         //public DbSet<IdentityRole> Roles { get; set; }
         public DbSet<Country> Countries { get; set; }
         public DbSet<Skill> Skills { get; set; }
+        public DbSet<TaskPriority> TaskPriorities { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
@@ -230,6 +231,13 @@ namespace MvcTaskManager.Identity
                 new Country() { CountryID = 191, CountryName = "Tuvalu" },
                 new Country() { CountryID = 192, CountryName = "Nauru" },
                 new Country() { CountryID = 193, CountryName = "Vatican City" }
+            );
+
+            modelBuilder.Entity<TaskPriority>().HasData(
+               new TaskPriority() { TaskPriorityID = 1, TaskPriorityName = "Urgent" },
+               new TaskPriority() { TaskPriorityID = 2, TaskPriorityName = "Normal" },
+               new TaskPriority() { TaskPriorityID = 3, TaskPriorityName = "Below Normal" },
+               new TaskPriority() { TaskPriorityID = 4, TaskPriorityName = "Low" }
             );
         }
     }
